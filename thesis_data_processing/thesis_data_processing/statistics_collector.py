2c6e7e6b74c2a1eed3e4d4e0ea49008af9a4e053
SuperJappie08 <36795178+SuperJappie08@users.noreply.github.com>
2025-07-29 20:54:17 +0200

    Add CI and small optimazation attempt

--- thesis_data_processing/thesis_data_processing/statistics_collector.py
+++ thesis_data_processing/thesis_data_processing/statistics_collector.py
@@ -10,13 +10,14 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-# limitations under the License
+# limitations under the License.
 from decimal import Decimal
 from typing import assert_type, Optional, TYPE_CHECKING
 
 import pandas as pd
 
 if TYPE_CHECKING:
+    from collections.abc import Hashable
     from types import ModuleType
 
     from pal_statistics_msgs.msg import StatisticsNames
@@ -35,7 +36,7 @@ logger = logging.getLogger(__name__)
 class StatisticsCollector:
     """A class to recombine 'statistics/names' and 'statistics/values'."""
 
-    def __init__(self, base_topic: str) -> None:
+    def __init__(self, base_topic: str, only_names: Optional[set[str]] = None) -> None:
         assert base_topic[-1] != '/', f"Invalid base topic '{base_topic}' must not end with '/'"
         assert not base_topic.endswith('/full'), (
             f"Invalid base topic '{base_topic}' must not end with '/full'"
@@ -47,9 +48,10 @@ class StatisticsCollector:
             f"Invalid base topic '{base_topic}' must not end with '/values'"
         )
         self.__base_topic = base_topic
+        self.__only_names = only_names
         self.__min_names_verion: Optional[int] = None
         self.__max_names_verion: Optional[int] = None
-        self.__names: list[tuple['StatisticsNames', set[str]]] = []
+        self.__names: list['StatisticsNames'] = []
         self.__values: list['StatisticsValues'] = []
         self.__data: pd.DataFrame = pd.DataFrame()  # columns=['time']
         self.__data.index.name = 'time'
@@ -121,25 +123,16 @@ class StatisticsCollector:
     def _process_names_msg(self, msg: 'StatisticsNames') -> None:
         if self.min_names_version is None:
             self.__min_names_verion = msg.names_version
-            self.__max_names_verion = msg.names_version
-            self.__names.append((msg, set()))
             self.__processed_names_until = 0
-        elif self.max_names_version < msg.names_version:
-            self.__max_names_verion = msg.names_version
-            old_names = set(self.__names[-1][0].names)
-            old_dropped = self.__names[-1][1]
-            new_names = set(msg.names)
-
-            not_returned = old_dropped - new_names
-
-            self.__names.append((msg, not_returned | (old_names - new_names)))
-        else:
+        elif not self.max_names_version < msg.names_version:
             logger.warning(
                 "Recieved new names for '%s', but the version has not increased. Skipping",
                 self.base_topic,
             )
             return
 
+        self.__max_names_verion = msg.names_version
+        self.__names.append(msg)
         logger.info(
             "Recieved new names for '%s' with version %d",
             self.base_topic,
@@ -166,10 +159,10 @@ class StatisticsCollector:
             self._update_dataframe()
 
     def _update_dataframe(self) -> bool:
-        """Update the internal DataFrame.
+        """
+        Update the internal DataFrame.
 
-        Returns:
-            bool: True if up-to-date
+        :returns bool: True if up-to-date
         """
         if self.__processed_values_until is None:
             if self.min_names_version is not None:
@@ -202,7 +195,13 @@ class StatisticsCollector:
             msg_time = Decimal(f'{msg.header.stamp.sec}.{msg.header.stamp.nanosec:0>9}')
             new_data = {name: float('nan') for name in self.__data.columns}
             new_data |= dict(
-                zip(self.__names[msg.names_version - self.min_names_version][0].names, msg.values),
+                filter(
+                    lambda kv: self._include_name(kv[0]),
+                    zip(
+                        self.__names[msg.names_version - self.min_names_version].names,
+                        msg.values,
+                    ),
+                ),
             )
 
             self.__data.loc[msg_time] = new_data
@@ -211,10 +210,10 @@ class StatisticsCollector:
         return True
 
     def _update_dataframe_headers(self) -> bool:
-        """Add new headers to the internal DataFrame.
+        """
+        Add new headers to the internal DataFrame.
 
-        Returns:
-            bool: True if up-to-date
+        :returns bool: True if up-to-date
         """
         if self.__processed_names_until is None:
             logger.error(
@@ -228,13 +227,16 @@ class StatisticsCollector:
                 self.base_topic,
             )
             return True
-        current_names_msgs, _ = self.__names[self.__processed_names_until]
+        current_names_msgs = self.__names[self.__processed_names_until]
 
         # if current_names_msgs
         for name in current_names_msgs.names:
             columns = self.__data.columns
-            if name not in columns:
+            if name not in columns and self._include_name(name):
                 self.__data.insert(len(columns), name, None)
 
         self.__processed_names_until += 1
         return True
+
+    def _include_name(self, name: 'str |Hashable') -> bool:
+        return self.__only_names is None or name in self.__only_names
